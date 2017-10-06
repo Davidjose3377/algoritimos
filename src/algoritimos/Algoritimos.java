e7f88b2b1d8aa01decd250839153509a507c8192
Aluno <Aluno@172.32.1.25>
2017-10-05 22:34:24 -0300

    criando arquivo teste

--- src/algoritimos/Algoritimos.java
+++ src/algoritimos/Algoritimos.java
@@ -1,33 +1,31 @@
-
 package algoritimos;
 
 public class Algoritimos {
-    
-    public static void quadrado (double n) {
-        
+
+    public static void quadrado(double n) {
+
         double r = n * n;
-        
+
     }
-    
-    public static double metade(double v){
-    
+
+    public static double metade(double v) {
+
         double r = v / 2;
         return r;
     }
 
     public static void main(String[] args) {
-        
+
         double raiz;
         raiz = Math.sqrt(15);
-        
+
         System.out.println("A raiz é " + raiz);
-        
+
         quadrado(raiz);
-        
+
         double result;
-        result = metade(15); 
-       
-       
+        result = metade(15);
+
     }
-    
+
 }
